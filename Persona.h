b25d4e03b6415ba3a21ace44d5edaf61d704f7b1
Jonatan <proggetoZ@gmail.com>
2022-05-11 16:04:06 -0300

    Delete Persona.h

--- Persona.h
+++
@@ -1,9 +0,0 @@
-#ifndef PERSONA_H
-#define PERSONA_H
-
-typedef struct persona {
-    int str; int des; int res; int in; int edu; int car;
-    char strH; char desH; char resH; char inH; char eduH; char carH;
-} persona;
-
-#endif
